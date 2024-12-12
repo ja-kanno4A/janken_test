dd654743c2c5ebf97f1b2511e267ee33809eb8e0
konnnoairi <a.konno.sys24@morijyobi.ac.jp>
2024-12-12 09:58:49 +0900

    computerのテストコードの作成 #2

--- tests/test_computer.py
+++ tests/test_computer.py
@@ -1,1 +1,21 @@
 import unittest
+from unittest.mock import patch
+from source.computer import computer_pon
+
+class TestComp(unittest.TestCase):
+
+    @patch('random.choice',side_effect=['グー'])
+    def test_1(self,mock_choice):
+        self.assertEqual(computer_pon(), "グー")
+
+    @patch('random.choice',side_effect=['チョキ'])
+    def test_2(self,mock_choice):
+        self.assertEqual(computer_pon(), "チョキ")
+
+    @patch('random.choice',side_effect=['パー'])
+    def test_3(self,mock_choice):
+        self.assertEqual(computer_pon(), "パー")
+
+    
+if __name__ == "__main__":
+    TestComp()
