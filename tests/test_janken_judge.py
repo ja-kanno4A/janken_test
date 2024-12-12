9959ffe1112f018ea3bcfa8d08e7d5bf57f1b2a9
konnnoairi <a.konno.sys24@morijyobi.ac.jp>
2024-12-12 11:44:41 +0900

    judgeのテストコードの作成 #3

--- tests/test_janken_judge.py
+++ tests/test_janken_judge.py
@@ -1,1 +1,26 @@
 import unittest
+from unittest.mock import patch
+from source.janken_judge import judge
+
+class TestJudge(unittest.TestCase):
+    def testpattern(self):
+        patterns = [
+           ('グー', 'グー','draw'),
+           ('グー', 'チョキ','computer_win'),
+           ('グー', 'パー','player_win'),
+           
+           ('チョキ', 'グー','player_win'),
+           ('チョキ', 'チョキ','draw'),
+           ('チョキ', 'パー','computer_win'),
+           
+           ('パー', 'グー','computer_win'),
+           ('パー', 'チョキ','player_win'),
+           ('パー', 'パー','draw'),
+       ]
+        
+        for c, p, j in patterns:
+           with self.subTest():
+                self.assertEqual(judge(c,p),j)
+
+if __name__ == "__main__":
+    TestJudge()
